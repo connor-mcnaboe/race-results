1fdfc8e1182b1cf2908bad37a250e68e6f5e1828
Connor McNaboe <connor.mcnaboe@uconn.edu>
2020-03-22 12:00:06 -0400

    RR-3: Form control support

--- src/pages/results-calculator-tab/results-calculator.module.ts
+++ src/pages/results-calculator-tab/results-calculator.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ResultsCalculatorPage } from "./results-calculator.page";
+import { ComponentsModule } from 'src/components/comoponent.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ComponentsModule,
     RouterModule.forChild([{ path: '', component: ResultsCalculatorPage }])
   ],
   declarations: [ResultsCalculatorPage]
